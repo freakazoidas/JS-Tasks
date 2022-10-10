63c5c2a56ed21909871c68a9de10366121b35deb
freakazoidas <freakazoidas@gmail.com>
2022-10-10 15:38:42 +0300

    3 task done, need to fix header

--- Task 2/script.js
+++ Task 2/script.js
@@ -3,3 +3,11 @@ Parašykite JS kodą, kuris skaičiuos kiek kartų buvo paspaustas mygtukas
 su tekstu "CLICK ME". Paspaudimų rezultatas turi būti matomas dešinėje
 pusėje esančiame "state" skaičiavimo bloke (<div id="btn__state">0</div>)
 ------------------------------------------------------------------- */
+let counter = document.getElementById("btn__element");
+let btn__state = document.getElementById("btn__state");
+let count = 0;
+
+counter.addEventListener("click", () => {
+  count += 1;
+  btn__state.innerHTML = count;
+});
--- Task 3/script.js
+++ Task 3/script.js
@@ -11,4 +11,74 @@ Pastaba: Sukurta kortelė, kurioje yra pateikiama vartotojo informacija, turi
 turėti bent minimalų stilių ir būti responsive;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'https://api.github.com/users';
+function retrieveContacts() {
+  const ENDPOINT = "https://api.github.com/users";
+  hideMessage();
+  fetch(ENDPOINT)
+    .then(function (response) {
+      // response.json() returns a json string,
+      // returning it will convert it
+      // to a pure JavaScript
+      // object for the next then's callback
+      return response.json();
+    })
+    .then(function (contacts) {
+      // contacts is a JavaScript object here
+      buildTable(contacts);
+    })
+    .catch(function (error) {
+      console.log("Error during fetch: " + error.message);
+    });
+}
+
+function hideClearButton() {
+  b = document.querySelector("#clear-button");
+  if (!b.classList.contains("hide")) {
+    b.classList.toggle("hide");
+  }
+}
+
+function clearTable() {
+  document.querySelector("#tableContactBody").innerHTML = "";
+  hideClearButton();
+}
+
+function showClearButton() {
+  b = document.querySelector("#clear-button");
+  b.classList.toggle("hide");
+}
+function hideMessage() {
+  b = document.querySelector("#message");
+  b.classList.add("hide");
+  c = document.querySelector("#tableHeader");
+  c.classList.remove("hide");
+}
+
+function buildTable(contacts) {
+  // Clear out the table body first.
+  clearTable();
+
+  // Add the table content.
+  // addLineToHTMLTable(element.fname, element.lname)
+  contacts.forEach(function (curr) {
+    addLineToHTMLTable(curr.login, curr.avatar_url);
+  });
+
+  showClearButton();
+}
+
+// Add a line to the HTML table
+function addLineToHTMLTable(loginVariable, avatarUrlVariable) {
+  // Get the body of the table using the selector API
+  var tableBody = document.querySelector("#tableContactBody");
+
+  // Add a new row at the end of the table
+  var newRow = tableBody.insertRow();
+
+  // add  new cells to the row
+  var loginVariableCell = newRow.insertCell();
+  loginVariableCell.innerHTML = loginVariable;
+
+  var avatarUrlVariableCell = newRow.insertCell();
+  avatarUrlVariableCell.innerHTML = avatarUrlVariable;
+}
