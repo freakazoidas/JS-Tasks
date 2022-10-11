57a691f7d4d63a87a4dfaae8c8c536852e2c2e6f
freakazoidas <freakazoidas@gmail.com>
2022-10-11 03:12:22 +0300

    8 done

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,70 @@ subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
 multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+// Not sure if this is ES6 or ES5
+
+const a = 4;
+const b = 5;
+
+class Calculator {
+  sum(a, b) {
+    return a + b;
+  }
+
+  subtraction(a, b) {
+    return a - b;
+  }
+
+  multiplication(a, b) {
+    return a * b;
+  }
+
+  division(a, b) {
+    if (b == 0) throw new Error("division by zero");
+    return a / b;
+  }
+}
+
+const cal = new Calculator();
+
+const sum = cal.sum(a, b);
+const subs = cal.subtraction(a, b);
+const mult = cal.multiplication(a, b);
+const div = cal.division(a, b);
+
+const object4methods = [sum, subs, mult, div];
+
+console.log(object4methods);
+
+/////////////////////////////
+
+// this is ES5?
+function calculate(number1, number2, operator) {
+  if (!number1 || !number2) {
+    return "Invalid number";
+  }
+  if (!["*", "-", "/", "+"].includes(operator)) {
+    return "Invalid operator";
+  }
+  switch (operator) {
+    case "+":
+      return number1 + number2;
+    case "/":
+      return number1 / number2;
+    case "*":
+      return number1 * number2;
+    case "-":
+      return number1 - number2;
+  }
+}
+console.log(calculate(2, 3, "+"));
+
+const sumF2 = (a, b) => calculate(a, b, "+");
+const subsF2 = (a, b) => calculate(a, b, "-");
+const multF2 = (a, b) => calculate(a, b, "*");
+const divF2 = (a, b) => calculate(a, b, "/");
+
+console.log(sumF2(2, 3));
+console.log(subsF2(2, 3));
+console.log(multF2(2, 3));
+console.log(divF2(2, 3));
